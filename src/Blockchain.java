fe37ab09c3fb6f8b29592f49c358494865938d60
LAPDev <dylan.360@hotmail.fr>
2021-04-25 09:12:34 +0200

    Add files via upload

---
+++ src/Blockchain.java
@@ -0,0 +1,251 @@
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+
+
+
+
+public class Blockchain {
+	
+	private int GENESIS = 0; // FALSE
+	private int DIFFICULTY = 0;
+	byte[] Originhash;
+	String s = "1GENESIS"; 
+	
+	// defaut genesis blk 'magic word' to sha-256
+	
+	// Default Constructor
+	
+	Blockchain()  {
+		
+        
+ 
+        
+        
+        
+		
+	}
+	
+
+	
+	// put a new block on CG blockchain 
+	public int setNewBlock() {
+		if (GENESIS == 0) {
+			// START AT GENESIS BLOCK
+			
+			
+			MessageDigest digest;
+			try {
+				digest = MessageDigest.getInstance("SHA-256");
+				Originhash = digest.digest(s.getBytes(Charset.forName("UTF-8")));
+				
+				
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			Path chemin = Paths.get("block1.hex");
+	        
+			 
+	        
+	        OutputStream output = null;
+	        try {
+	            // Un objet BufferedOutputStream est affecté à la référence OutputStream.
+	            output = new BufferedOutputStream(Files.newOutputStream(chemin));
+	            // Ecrire dans le fichier
+	            output.write(Originhash);
+	 
+	            // vider le tampon
+	            output.flush();
+	 
+	            // fermer le fichier
+	            output.close();
+	 
+	        } catch (Exception e) {
+	            System.out.println("Message " + e); }
+	        
+	       
+			
+		}
+		
+		else {
+			// IF GENESIS BLOCK ALREADY MINED
+			
+			// APPEND NEW BLOCK
+			
+			Path chemin = Paths.get("block2.hex");
+	        
+			 
+	        
+	        OutputStream output = null;
+	        try {
+	            // Un objet BufferedOutputStream est affecté à la référence OutputStream.
+	            output = new BufferedOutputStream(Files.newOutputStream(chemin));
+	            // Ecrire dans le fichier
+	            output.write(Originhash);
+	 
+	            // vider le tampon
+	            output.flush();
+	 
+	            // fermer le fichier
+	            output.close();
+	 
+	        } catch (Exception e) {
+	            System.out.println("Message " + e); }
+			
+			
+			
+			
+		}
+		
+		
+		return 0;
+	}
+	
+	int basicMining() {
+		// TRUE HASHING
+		while (Originhash[0] != '0') {
+			
+			// Basic mining work !
+			
+			MessageDigest digest;
+			
+			
+			try {
+				digest = MessageDigest.getInstance("SHA-256");
+				Originhash = digest.digest(s.getBytes(Charset.forName("UTF-8")));
+				s = s+1;
+				System.out.println(s);
+				System.out.println(Originhash);
+				
+				Path chemin = Paths.get("block1HASH.hex");
+		        
+				 
+		        
+		        OutputStream output = null;
+		        try {
+		            // Un objet BufferedOutputStream est affecté à la référence OutputStream.
+		            output = new BufferedOutputStream(Files.newOutputStream(chemin));
+		            // Ecrire dans le fichier
+		            output.write(Originhash);
+		 
+		            // vider le tampon
+		            output.flush();
+		 
+		            // fermer le fichier
+		            output.close();
+		 
+		        } catch (Exception e) {
+		            System.out.println("Message " + e); }
+				
+				
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		// END GENESIS MINING
+		
+		GENESIS = 1; // TRUE
+		
+		
+		
+		
+		DIFFICULTY++; // Increase difficulty after first block mined
+		
+		return 0;
+	}
+	
+	int setMining(int methodDIFFICULTY) throws IOException {
+		int standardBlockDifficulty;
+		standardBlockDifficulty = DIFFICULTY;
+		System.out.println(standardBlockDifficulty);
+		
+		MessageDigest digest;
+		
+		
+			Path cheminBlock = Paths.get("block1HASH.hex");
+	        
+			Scanner myReader = new Scanner(cheminBlock);
+			  while (myReader.hasNextLine()) {
+			    String hashFirstBlock = myReader.nextLine();
+			    System.out.println(hashFirstBlock);
+			    
+			    byte[] hashFirstBlockTab = (s.getBytes(Charset.forName("UTF-8")));
+			    
+			    // Loop second block mining
+			    
+			    while (hashFirstBlockTab[0] != '0' ) {
+			    	
+			    	try {
+						digest = MessageDigest.getInstance("SHA-256");
+						hashFirstBlockTab = digest.digest(hashFirstBlock.getBytes(Charset.forName("UTF-8")));
+						hashFirstBlock = hashFirstBlock+1;
+						System.out.println(hashFirstBlock);
+						System.out.println(hashFirstBlockTab);
+						
+						Path chemin = Paths.get("block2HASH.hex");
+				        
+						 
+				        
+				        OutputStream output = null;
+				        try {
+				            // Un objet BufferedOutputStream est affecté à la référence OutputStream.
+				            output = new BufferedOutputStream(Files.newOutputStream(chemin));
+				            // Ecrire dans le fichier
+				            output.write(Originhash);
+				 
+				            // vider le tampon
+				            output.flush();
+				 
+				            // fermer le fichier
+				            output.close();
+				 
+				        } catch (Exception e) {
+				            System.out.println("Message " + e); }
+						
+						
+					} catch (NoSuchAlgorithmException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			    	
+			    }
+			    
+			    
+			    
+			  }
+			  myReader.close();
+		
+		
+		return 0;
+		
+	}
+	
+	int setNewTransaction() {
+		return 0;
+	}
+	
+	
+	
+
+}
---
+++ src/Start.java
@@ -0,0 +1,30 @@
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.*;
+
+public class Start {
+
+	public static void main(String[] args) throws IOException  {
+	
+	// basic GUI
+	JFrame frame = new JFrame("CryptoGlobal CORE ALPHA 0.1");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(600,400);
+    frame.setVisible(true);
+    
+    
+    
+  
+    
+    // start new blockchain
+    Blockchain blk = new Blockchain();
+    blk.setNewBlock();
+    blk.basicMining();
+    blk.setMining(0);
+    
+	}	
+
+}
